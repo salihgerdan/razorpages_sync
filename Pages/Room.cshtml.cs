ebb5541a957199c6f93bac6d6c11089d3172e839
Salih Gerdan <msalih45@gmail.com>
2022-11-15 16:51:05 +0300

    Initial upload

---
+++ Pages/Room.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_pages.Pages;
+
+public class RoomModel : PageModel
+{
+    private readonly ILogger<RoomModel> _logger;
+
+    public RoomModel(ILogger<RoomModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function runTimer() {
+    let timer = document.getElementById("remainingTime");
+    let remTime = timer.innerHTML.split(":");
+    let remMinutes = parseInt(remTime[0]);
+    let remSeconds = parseInt(remTime[1]);
+    let totalSeconds = remMinutes * 60 + remSeconds;
+    if (totalSeconds > 0) {
+        totalSeconds -= 1;
+        remMinutes = Math.floor(totalSeconds / 60);
+        remSeconds = totalSeconds % 60;
+        timer.innerHTML = `${remMinutes}:${remSeconds}`;
+        setTimeout(runTimer, 1000);
+    }
+}
